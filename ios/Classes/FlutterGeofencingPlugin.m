78d72962b42ed2535f8e135705f7d9ff41b97036
Kresten Vester <JFreakDK@users.noreply.github.com>
2019-10-23 12:54:01 +0200

    Revert "Add swift"

--- ios/Classes/FlutterGeofencingPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "FlutterGeofencingPlugin.h"
-#import <flutter_geofencing/flutter_geofencing-Swift.h>
-
-@implementation FlutterGeofencingPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterGeofencingPlugin registerWithRegistrar:registrar];
-}
-@end
